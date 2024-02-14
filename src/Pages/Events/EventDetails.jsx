c2faa0a5ddab17602d3dab1d8008313145cb7f3b
Deven Umrania <umraniadeven10@gmail.com>
2024-02-15 01:01:04 +0530

    update: modified design for event details page

--- src/Pages/Events/EventDetails.jsx
+++ src/Pages/Events/EventDetails.jsx
@@ -1,45 +1,51 @@
 import { useState } from "react";
 import Modal from "react-bootstrap/Modal";
+import { formatDate } from "../../../utils/utilityFunctions";
 
 function MyVerticallyCenteredModal({ show, onHide, event }) {
   return (
-    <Modal show={show} onHide={onHide} size="sm" centered>
+    <Modal show={show} onHide={onHide} size="md" centered>
       <Modal.Body className="text-center px-4">
         <div className="d-flex justify-content-start gap-2">
-          <p className="m-0" style={{ fontWeight: "600" }}>
-            Name:
-          </p>
-          <p className="m-0">{event.name}</p>
+          <h3 className="m-0" style={{ fontWeight: "600" }}>
+            {event.name}
+          </h3>
         </div>
-        <div className="d-flex justify-content-start gap-2">
+        <div className="d-flex justify-content-start gap-2 mt-3">
           <p className="m-0" style={{ fontWeight: "600" }}>
             Date:
           </p>
-          <p className="m-0">{event.date}</p>
+          <p className="m-0">{formatDate(event.date)}</p>
         </div>
-        <div className="d-flex justify-content-start gap-2">
+        <div className="d-flex flex-column justify-content-start align-items-start mt-2">
           <p className="m-0" style={{ fontWeight: "600" }}>
             Location:
           </p>
           <p className="m-0">{event.location}</p>
         </div>
-        <div className="d-flex justify-content-start gap-2">
+        <div className="text-start d-flex flex-column justify-content-start align-items-start mt-2">
           <p className="m-0" style={{ fontWeight: "600" }}>
             Description:
           </p>
           <p className="m-0">{event.description}</p>
         </div>
-        <div className="d-flex justify-content-start gap-2">
+        <div className="d-flex justify-content-start gap-2 mt-2">
           <p className="m-0" style={{ fontWeight: "600" }}>
             Volunteers Required:
           </p>
           <p className="m-0">{event.volunteersNumber} people</p>
         </div>
-        <div className="d-flex justify-content-start gap-2">
+        <div className="text-start d-flex flex-column justify-content-start mt-2">
           <p className="m-0" style={{ fontWeight: "600" }}>
             Volunteer Roles:
           </p>
-          <p className="m-0">{event.volunteerRoles}</p>
+          <div className="mt-1 d-flex flex-wrap gap-2 justify-content-start align-items-start">
+            {event?.volunteerRoles?.map((role) => (
+              <p className="m-0 role badge" key={role}>
+                {role}
+              </p>
+            ))}
+          </div>
         </div>
 
         <button className="custom-btn mt-3" onClick={onHide}>
