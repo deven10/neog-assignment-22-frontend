adf0291a842f7a9fb75ad508da0220113f1709bf
Deven Umrania <umraniadeven10@gmail.com>
2025-01-18 15:10:58 +0530

    update: 1) integrated TS 2) renamed files from js to ts/tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App.js";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./Store/store";
+import store from "./Store/store.js";
 
 import "./index.css";
 
